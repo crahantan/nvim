a64b6bf30cf37622484403dc93b5e279934432cb
Luis Alvarez <crahantandev@gmail.com>
2025-06-13 10:24:04 -0600

    Fix: Change C-Return accept suggestion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -26,7 +26,7 @@ return {
 				debounce = 75,
 				trigger_on_accept = true,
 				keymap = {
-					accept = "<S-j>",
+					accept = "<S-Return>",
 					accept_word = false,
 					accept_line = false,
 					next = "<M-]>",
