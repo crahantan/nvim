c7d447cedcd90c9ccc6064723c7101e1db71da3d
Luis Álvarez <crahantandev@gmail.com>
2026-03-19 17:03:37 -0600

    fix: Set copilot disable for default

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
 				bash = { "beautysh" },
 				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
+				python = { "isort" },
 				-- You can customize some of the format options for the filetype (:help conform.format)
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
